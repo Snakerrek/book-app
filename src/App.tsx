b7832f1392bd3cd1c112f316d1b49c17510f0f50
Tomasz <snakerrek@gmail.com>
2022-08-11 16:58:17 +0200

    Basic router setup with some pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <div>App</div>;
-}
+import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import BookList from "./pages/BookList";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/bookList">Book list</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/bookList" element={<BookList />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
